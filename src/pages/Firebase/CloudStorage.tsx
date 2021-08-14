b7e331c6e052ed8c8ec517eb74dbef8e04d04e0c
crypt0box <pray.happy76@gmail.com>
2021-08-14 11:14:02 +0900

    feat: implement uploading image url to firestore feature

--- src/pages/Firebase/CloudStorage.tsx
+++ src/pages/Firebase/CloudStorage.tsx
@@ -9,6 +9,7 @@ import {
   Input,
   Image,
   Button,
+  HStack,
 } from '@chakra-ui/react'
 
 export type firebaseOnLoadProp = {
@@ -17,68 +18,92 @@ export type firebaseOnLoadProp = {
   // このほかにもmetadata,task,refがある
 }
 
+type ImageProps = {
+  imageId: string
+  imageUrl: string
+}
+
 const CloudStorage = () => {
-  const [loacalImage, setLocalImage] = useState<string>()
-  const [image, setImage] = useState<File>()
-  const [imageUrl, setImageUrl] = useState('')
+  const [localImageList, setLocalImageList] = useState<ImageProps[]>()
+  const [imageList, setImageList] = useState<File[]>()
   const userRef = db.collection('users').doc(`${auth.currentUser?.uid}`)
 
+  const generateKey = (pre: string) => {
+    return `${pre}_${new Date().getTime()}`
+  }
+
   const handleImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return
     const imageFile = event.target.files?.[0]
-    const localImageUrl = URL.createObjectURL(imageFile)
-    setImage(imageFile)
-    setLocalImage(localImageUrl)
+    const localImage = {
+      imageId: generateKey('image'),
+      imageUrl: URL.createObjectURL(imageFile),
+    }
+
+    if (!imageList) {
+      setImageList([imageFile])
+    } else {
+      setImageList([imageFile, ...imageList])
+    }
+
+    if (!localImageList) {
+      setLocalImageList([localImage])
+    } else {
+      setLocalImageList([localImage, ...localImageList])
+    }
   }
   const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
-    if (!image) {
+    if (!imageList?.length) {
       console.log('ファイルが選択されていません')
       return
     }
     // アップロード処理
-    if (!auth.currentUser) return
-    const uploadTask = storage
-      .ref(`/images/${auth.currentUser.uid}/${image.name}`)
-      .put(image)
-    uploadTask.on(
-      Firebase.storage.TaskEvent.STATE_CHANGED,
-      next,
-      error,
-      complete
-    )
+    const urls: string[] = []
+    imageList?.forEach((image) => {
+      if (!auth.currentUser) return
+      const uploadTask = storage
+        .ref(`/images/${auth.currentUser.uid}/${image.name}`)
+        .put(image)
+      uploadTask.on(
+        Firebase.storage.TaskEvent.STATE_CHANGED,
+        next,
+        error,
+        () => {
+          // 完了後の処理
+          // 画像表示のため、アップロードした画像のURLを取得
+          if (!image || !auth.currentUser) return
+          storage
+            .ref(`images/${auth.currentUser.uid}`)
+            .child(image.name)
+            .getDownloadURL()
+            .then((fireBaseUrl) => {
+              urls.push(fireBaseUrl)
+              userRef.set(
+                {
+                  image: urls,
+                },
+                { merge: true }
+              )
+            })
+            .catch((error) => {
+              console.log('CloudStorage.tsx Error', error)
+            })
+        }
+      )
+    })
   }
   const next = (snapshot: firebaseOnLoadProp) => {
     // 進行中のsnapshotを得る
     // アップロードの進行度を表示
     const percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
     console.log(percent + '% done')
-    console.log(snapshot)
   }
   const error = (error: any) => {
     // エラーハンドリング
     console.log(error)
   }
-  const complete = () => {
-    // 完了後の処理
-    // 画像表示のため、アップロードした画像のURLを取得
-    if (!image || !auth.currentUser) return
-    storage
-      .ref(`images/${auth.currentUser.uid}`)
-      .child(image.name)
-      .getDownloadURL()
-      .then((fireBaseUrl) => {
-        userRef.set(
-          {
-            image: [`${fireBaseUrl}`],
-          },
-          { merge: true }
-        )
-        setImageUrl(fireBaseUrl)
-      })
-      .catch((error) => {
-        console.log('CloudStorage.tsx Error', error)
-      })
-  }
+
   return (
     <Flex w="100vw" h="100vh" justify="center" align="center">
       <Box>
@@ -88,9 +113,18 @@ const CloudStorage = () => {
           <Input hidden type="file" accept="image/*" onChange={handleImage} />
         </FormControl>
         <Button onClick={handleSubmit}>SUBMIT</Button>
-        {loacalImage && (
-          <Image src={loacalImage} w="400px" h="360px" objectFit="contain" />
-        )}
+        <HStack>
+          {localImageList?.length &&
+            localImageList.map((localImage) => (
+              <Image
+                src={localImage.imageUrl}
+                key={localImage.imageId}
+                w="150px"
+                h="150px"
+                objectFit="contain"
+              />
+            ))}
+        </HStack>
       </Box>
     </Flex>
   )
